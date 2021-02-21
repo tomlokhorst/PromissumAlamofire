5c7a28881a6a92c748f077ab5511d464a9bcfb4d
Tom Lokhorst <tom@lokhorst.eu>
2021-02-21 20:27:12 +0100

    Remove Promissum code

--- Package.swift
+++ Package.swift
@@ -2,23 +2,19 @@
 import PackageDescription
 
 let package = Package(
-  name: "Promissum",
+  name: "PromissumAlamofire",
   platforms: [
     .macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)
   ],
   products: [
-    .library(name: "Promissum", targets: ["Promissum"]),
     .library(name: "PromissumAlamofire", targets: ["PromissumAlamofire"]),
-    .library(name: "PromissumUIKit", targets: ["PromissumUIKit"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.1.0")
+    .package(url: "https://github.com/tomlokhorst/Promissum.git", .branch("6")),
+    .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.1.0"),
   ],
   targets: [
-    .target(name: "Promissum"),
     .target(name: "PromissumAlamofire", dependencies: ["Promissum", "Alamofire"]),
-    .target(name: "PromissumUIKit", dependencies: ["Promissum"]),
-    .testTarget(name: "PromissumTests", dependencies: ["Promissum"]),
     .testTarget(name: "PromissumAlamofireTests", dependencies: ["PromissumAlamofire"]),
   ],
   swiftLanguageVersions: [.v5]
